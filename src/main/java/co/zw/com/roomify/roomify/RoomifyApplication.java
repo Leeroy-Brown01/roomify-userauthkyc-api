4d4f94de47c35116a5ab40229af510866ef9c44f
Leeroy Brown <brownleeroy010@gmail.com>
2026-02-11 14:58:04 +0200

    My first commit

---
+++ src/main/java/co/zw/com/roomify/roomify/RoomifyApplication.java
@@ -0,0 +1,13 @@
+package co.zw.com.roomify.roomify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomifyApplication.class, args);
+	}
+
+}
